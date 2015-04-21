b91f40f6c3c6293ceae9d6b5ad9a2a52d18f8407
= <bongostar41@gmail.com>
2015-04-20 21:35:41 -0400

    Custom Directives pt 2

--- Lesson05/src/app/main/main.controller.js
+++ Lesson05/src/app/main/main.controller.js
@@ -14,10 +14,75 @@ angular.module('gulpTemplate')
     restrict: 'A'
   };
 })
-  
-// .controller('MainCtrl', function () {
 
-// })
+.directive ('gdBackgroundChange', function() {
+  return {
+    templateUrl: 'app/partials/gd-background-change.html',
+    controller: 'GDBackgroundChangeCtrl as bgCtrl'
+  };
+})
+  
+.controller('GDBackgroundChangeCtrl', function () {
+  var self = this;
+  self.isOn = true;
+})
+
+.directive('gdPersonDisplay', function() {
+  return {
+    templateUrl: 'app/partials/gd-person-display.html',
+    scope: false
+  };
+})
+
+.directive('gdPersonValue', function() {
+  return {
+    templateUrl: 'app/partials/gd-person-value.html',
+    scope: {
+      firstName: '@',
+      lastName: '@'
+    }
+  };
+})
+
+.directive('gdPersonReference', function () {
+  return {
+    templateUrl: 'app/partials/gd-person-reference.html',
+    scope: {
+      person: '=',
+      onRemove: '&'
+    },
+    controller:'PCtrl'
+  };
+})
+.controller('PCtrl', function($scope) {
+  
+})
+
+.controller('MainCtrl', function() {
+  var self = this;
+
+  self.person1 = {
+    fname: 'Frank',
+    lname: 'Sinatra'
+  };
+  self.person2 = {
+    fname: 'Bill',
+    lname: 'Clinton'
+  };
+  self.person3 = {
+    fname: 'Snoop',
+    lname: 'Doggy Dogg'
+  };
+
+  self.removePerson = function(person) {
+  	var index = self.persons.indexOf(person);
+  	if (index >= 0) {
+  		self.persons.splice(index, 1);
+  	}
+  }
+
+  self.persons = [self.person1, self.person2, self.person3];
+})
 
 
 
