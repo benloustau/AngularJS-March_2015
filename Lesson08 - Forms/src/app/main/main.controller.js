00dc743c9c59b2cecbf01e686bcb16e7b9eb938b
Charles Castillo <charlescastillo1982@yahoo.com>
2015-05-04 20:09:09 -0400

    Forms (cont)

--- Lesson08 - Forms/src/app/main/main.controller.js
+++ Lesson08 - Forms/src/app/main/main.controller.js
@@ -2,13 +2,53 @@
 
 angular.module('lesson08')
 
-.controller('MainCtrl', function ($scope) {
+.value('SimpleArray', [
+	"Frank Sinatra",
+	"Tom Ford", 
+	"Bill Clinton"
+])
+
+.value('STATES', [{
+		abbr: 'CA',
+		displayName: 'California'
+	}, {
+		abbr: 'FL',
+		displayName: 'Florida'
+	}, {
+		abbr: 'OR',
+		displayName: 'Oregon'
+}])
+
+.value('STATE_OBJECTS', {
+	kCA: {
+		abbr: 'CA',
+		displayName: 'California'
+	},
+	kFL: {
+		abbr: 'FL',
+		displayName: 'Florida'
+	},
+	kOR: {
+		abbr: 'OR',
+		displayName: 'Oregon'
+	}
+})
+
+.controller('MainCtrl', function ($scope, SimpleArray, STATES, STATE_OBJECTS) {
 	var self = this;
+
+	self.selectOptions = STATE_OBJECTS;
+
+	self.isFirstNameRequired = true;
 
 	self.submitForm = function() {
 		if (self.signUpForm.$valid) {
 			console.log("Success")
 		}
+	}
+
+	self.logFname = function(name) {
+		console.log(name);
 	}
 
 	$scope.$watch('ctrl.email', function(newEmailValue) {
